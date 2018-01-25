c4a17d9bd966b5d5cddd67de223adda5e11cec41
Tomasz Klim <github@tomaszklim.pl>
2018-01-25 01:16:02 +0100

    fixed error messages

--- setup.sh
+++ setup.sh
@@ -10,10 +10,10 @@ if [ ! -f $cfg ]; then
 
 	if [ "$NEWRELIC_LICENSE" != "" ]; then
 		if [ ${#NEWRELIC_LICENSE} != 40 ]; then
-			echo "error: invalid license key length, aborting newrelic-sysmond setup"
+			echo "error: invalid license key length, aborting"
 			exit 1
 		elif ! [[ $NEWRELIC_LICENSE =~ ^[a-z0-9]+$ ]]; then
-			echo "error: license key contains invalid characters, aborting newrelic-sysmond setup"
+			echo "error: license key contains invalid characters, aborting"
 			exit 1
 		fi
 	fi
